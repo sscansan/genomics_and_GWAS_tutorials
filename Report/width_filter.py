55d66eca1e0a0e43905e715b92ebbd9c7f24d039
Stefano Scansani <75729718+sscansan@users.noreply.github.com>
2024-03-22 14:39:02 +0100

    Update width_filter.py

--- Report/width_filter.py
+++ Report/width_filter.py
@@ -1,12 +1,11 @@
 import re
 
-# Read the LaTeX file
 with open('output.tex', 'r') as file:
     latex_content = file.read()
 
-# Add [width=\linewidth] between \includegraphics and the image path
+# add [width=\linewidth] between \includegraphics and the image
 modified_latex_content = re.sub(r'(\\includegraphics)(\{[^}]+\})', r'\1[width=\\linewidth]\2', latex_content)
 
-# Write the modified LaTeX content back to the file
+# add it to the file
 with open('output.tex', 'w') as file:
     file.write(modified_latex_content)
